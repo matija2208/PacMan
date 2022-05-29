429cd274d6401eef7b3a91eb5f3f1bf154ef643c
matija2208 <matijacetrovic@gmail.com>
2022-05-29 20:33:57 +0200

    INITIAL COMMIT

---
+++ pacman/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿
+namespace pacman
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(796, 447);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.Name = "Form1";
+            this.Text = "Pac-Man";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
---
+++ pacman/Form1.cs
@@ -0,0 +1,72 @@
+
+namespace pacman
+{
+    public partial class Form1 : Form
+    {
+        int height;
+        int width;
+        string[] maze;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int speed = 1;
+            //Bitmap pac = new Bitmap(Image.FromFile(@"C:/Users/matij/source/repos/pacman/pacman/pacman1.png"), new Size(96,96));
+            
+            Graphics g = this.CreateGraphics();
+            
+            if(e.KeyCode == Keys.Up)
+            {
+                Crtaj();
+            }
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            height=this.Height;
+            width=this.Width;
+            Console.WriteLine(height + " " + width);
+            Load_From_File();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            height = this.Height;
+            width = this.Width;
+            Console.WriteLine("resize: "+height + " " + width);
+            Crtaj();
+        }
+
+        private void Load_From_File()
+        {
+            maze = System.IO.File.ReadAllLines(@"C:\Users\matij\source\repos\pacman\pacman\maze1.txt");
+            foreach(string i in maze)
+                Console.WriteLine(i);
+        }
+        private void Crtaj()
+        {
+            int dim = ((height-39) / maze.Length > width / maze[0].Length)? width / maze[0].Length: (height-39) / maze.Length;
+            int xMove = (width - dim * maze[0].Length)/2;
+            int yMove = (height - dim * maze.Length - 39)/2;
+            Graphics g = this.CreateGraphics();
+            g.FillRectangle(new SolidBrush(Color.Black), 0, 0, width, height);
+            Bitmap walltest = new Bitmap(Image.FromFile(@"C:/Users/matij/source/repos/pacman/pacman/images/test_wall.png"), new Size(dim, dim));
+            Console.WriteLine("CharDimmens: " + maze.Length + " x " + maze[0].Length+"\t"+dim);
+            for (int i=0; i < maze.Length; i++)
+            {
+                for(int j=0; j < maze[i].Length; j++)
+                {
+                    if (maze[i][j]=='#')
+                    {
+                        g.DrawImage(walltest, j * dim + xMove, i * dim + yMove);
+                    }
+                }
+            }
+        }
+    }
+}
